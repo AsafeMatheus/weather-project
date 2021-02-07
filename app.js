99451b15910ec96a57480f4ceae9c14ee490bc54
Asafe Matheus <asafesousa66@outlook.com>
2021-02-06 22:34:00 -0300

    update 1

--- app.js
+++ app.js
@@ -11,18 +11,42 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 app.post('/', (req, res) => {
-    const apiKey = 'your api key'
+    const apiKey = 'your API key'
+    const city = req.body.cityName
 
-    console.log(req.body)
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Brasilia&units=metric&appid=' + apiKey
-
-    res.send('Server is running')
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + apiKey
 
     https.get(url, (response) => {
-        console.log(response.statusCode)
+        console.log('status code: ' +response.statusCode)
         response.on('data', (data) => {
             const weatherData = JSON.parse(data)
-            console.log('climate in brasilia:' + weatherData.main.temp)
+            const emoji = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+
+            res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Weather Project</title>
+
+                <link rel="stylesheet" href="style.css">
+            </head>
+            <body>
+                <main>
+                    <img src="${emoji}">
+                    <h1>${weatherData.name} </h1>
+                    <hr>
+                    <ul>
+                        <li>country: ${weatherData.sys.country}</li>
+                        <li>temperature: ${weatherData.main.temp}°C</li>
+                        <li>feels like: ${weatherData.main.feels_like}°C</li>
+                    </ul>
+                </main>
+            </body>
+            </html>
+            `)
         })
     })
 })
@@ -30,4 +54,3 @@ app.post('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on door 3000.')
 })
-a
